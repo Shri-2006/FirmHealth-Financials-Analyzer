6164f4e026cd61cfe1ced6054a1556d6002cd985
Shriyans Singh <shriyans.singh@stonybrook.edu>
2026-06-18 08:11:10 +0000

    Phase 3 app.py is updated with a /train route and subprocess call for training model

--- ml_service/app.py
+++ ml_service/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from ingestor import fetch_metrics
 from sanitizer import sanitize_csv
-
+import subprocess
 app = Flask(__name__)
 
 @app.route('/health',methods=['GET'])
@@ -25,6 +25,17 @@ def csv_ingest():
     if metrics is None:
         return jsonify({"error":"Failed to fetch csv metrics"}),500
     return jsonify(metrics)
+
+@app.route('/train',methods=['POST'])
+def train_model():
+    try:
+        file=request.files['file'] 
+        file.save('data/custom_training.csv')
+        subprocess.run(["python3","train_model.py"])
+        return jsonify({"status":"training complete"})
+    except:
+        return jsonify({"error":"Failed to train on sample csv metrics"}),500
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
